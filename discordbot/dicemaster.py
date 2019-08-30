9bb7d7a5e0e51ad673567f841302ddb826caffaf
Wayman <wayman@waymantech.com>
2019-08-29 21:54:43 -0500

    The Bot now generates random numbers and sends them to channel, no sterilization of input

--- discordbot/dicemaster.py
+++ discordbot/dicemaster.py
@@ -1,5 +1,19 @@
 import discord
+from random import randint
 from tokenfile import TOKEN
+
+def rollDice(msg):
+    contents = msg.content.split(' ')
+    rollrequest = contents[1].split('d')
+    dicequant = rollrequest[0]
+    dicesize = rollrequest[1]
+    rollresults = ''
+    x=1
+    while x <= int(dicequant):
+        rollednum = randint(1, int(dicesize))
+        rollresults += '{} '.format(rollednum)
+        x+=1
+    return rollresults
 
 client = discord.Client()
 
@@ -14,5 +28,13 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('!roll'):
+        await message.channel.send(message.author.name + " rolled " + rollDice(message))
+
+    if message.content.startswith('!showmewhatyougot'):
+        await message.channel.send(message)
+        await message.channel.send(message.content)
+        await message.channel.send(message.author.name)
 
 client.run(TOKEN)
